5bd3bf8c4c209038a93b33f041a78715d28d7772
Jennifer Pazos <jmp521@gmail.com>
2021-05-21 14:41:33 -0500

    commit tree 6f44ab2f78a1a8148d242250bf3341aa7dc37aa0

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
-// Code EyesOnMe Component Here
+import React, {Component} from 'react';
+import { RuntimeGlobals } from 'webpack';
+
+class EyesOnMe extends Component {
+
+    handleOnBlur = ()= > {
+        
+    }
+
+    render() {
+        return(
+            <div>
+                <button onFocus={this.handleOnFocus} onBlur={this.handleOnBlur}></button>
+            </div>
+        )
+    }
+}
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
-// Code Keypad Component Here
+import React, {Component} from 'react';
+
+class Keypad extends Component {
+    handleOnChange = (e) => {
+        e.preventDefault()
+        console.log("Entering Password...")
+    }
+    render() {
+        return(
+            <div>
+                <input type="password" keyUp={this.handleOnChange} />
+            </div>
+        )
+    }
+}
+
+export default Keypad
